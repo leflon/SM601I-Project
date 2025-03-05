3e1ab786acf1970c8dc393c74d873e979b535d27
Paul Leflon <leflon.paul@gmail.com>
2025-03-05 12:38:02 +0100

    refactor(ScheduleGraph): remove old unused table rendering code

--- ScheduleGraph.py
+++ ScheduleGraph.py
@@ -63,58 +63,6 @@ class ScheduleGraph:
         # We want to display all asterisks and the very first cell in dark gray for better readability.
         print_matrix(adapted_matrix, lambda render, val, i, j: dark_gray(render) if val == '*' or i == j == 0 else render)
 
-        return
-        CELL_PADDING = 2 # Padding added at the left and right of each cell, for better readability.
-        # This will be useful to render the top row and left column.
-        vertex_name = lambda index: 'α' if index == 0 else 'ω' if index == N - 1 else str(index)
-        top = '╔' # Top border of the table
-        sep = '╠' # Separator between each row
-        bot = '╚' # Bottom border of the table
-        col_lengths = [] # Represents the length of each column, based on the length of its longuest cell
-        # This generates the top, bottom, and separator lines to correctly align with the size of each cell.
-        for i in range(N + 1): # + 1 to account for the heading column
-            if (i == 0): # This is the heading column, the cells will simply contain names of each vertex, which are merely the indexes of the array.
-                max_cell_length = len(str(N - 1)) + CELL_PADDING * 2 # -1 is added in case the last cell index adds a new digit (10, 100, etc.). 
-                                                                     # Since it's replaced by omega, one character, this avoids unnecessary big cells, if that ever happens
-            else:
-                # Iterating through the current column, we keep the longuest cell length to correctly align every row
-                max_cell_length = max([len(str(self.matrix[j][i - 1])) if self.matrix[j][i - 1] != None else 1 for j in range(N)]) + CELL_PADDING * 2
-            col_lengths.append(max_cell_length)
-            line = '═' * max_cell_length
-            top += line + '╦'
-            sep += line + '╬'
-            bot += line + '╩'
-        # We replace the last character with a closing character instead of the previous connecting ones
-        top = top[:-1] + '╗'
-        sep = sep[:-1] + '╣'
-        bot = bot[:-1] + '╝'
-        print(top)
-        header_row = '║'
-        # This loops generates the header row, displaying the name of each column
-        for i in range(N + 1): # Since we have a "header" column as well, the very top-left corner is left empty.
-            if i == 0: # Empty corner
-                header_row+= dark_gray('\\'.center(col_lengths[0], ' ')) + '║'
-            else:
-                name = vertex_name(i - 1) # since the first iteration is done on the empty corner, everything is shifted one cell to the right.
-                header_row += bold(name.center(col_lengths[i], ' ')) + '║'
-        print(header_row)
-        print(sep)
-        # This loop displays each row of the table
-        for i in range(N):
-            # First, it displays the name of each row
-            line = self.matrix[i]
-            name = vertex_name(i)
-            row = '║' + bold(name.center(col_lengths[0], ' ')) + '║'
-            # This loop displays the rest of the row
-            for j in range(N):
-                cell = str(line[j]) if line[j] != None else '*'
-                padded = cell.center(col_lengths[j + 1], ' ')
-                row+= (dark_gray(padded) if cell == '*' else padded) + '║'
-            print(row)
-            if i + 1 < N:
-                print(sep)
-        print(bot)
-
 
     def check(self, display_result=False) -> bool:
         """
